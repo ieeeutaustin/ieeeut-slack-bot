cb4f4075f0cdb330ce916f87c3de3c1745ef0b8a
Chang <andrewchang@utexas.edu>
2023-07-20 11:05:51 -0500

    format event block time

---
+++ blocks/events.js
@@ -0,0 +1,77 @@
+const { format } = require("date-fns");
+
+const formatTime = (date) => {
+    return format(date, 'h:mm a')
+}
+
+const formatDate = (date) => {
+    return format(date, 'eeee, MMMM do, yyyy')
+}
+
+// weekly event header
+const createHeaderBlock = (startOfWeek, endOfWeek) => {
+  headerBlock = [
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: ":tada: *EVENTS THIS WEEK* :tada:",
+      },
+    },
+    {
+      type: "context",
+      elements: [
+        {
+          text: `*${startOfWeek} - ${endOfWeek}*`,
+          type: "mrkdwn",
+        },
+      ],
+    },
+  ];
+  return headerBlock;
+};
+
+// event block
+const createEventBlock = (event) => {
+  const start = new Date(event.start.dateTime);
+  const end = new Date(event.end.dateTime);
+
+  const startTime = formatTime(start)
+  const endTime = formatTime(end)
+  const eventDate = formatDate(start)
+
+  eventBlock = [
+    {
+      type: "divider",
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `*${event.summary}*`,
+      },
+    },
+    {
+      type: "context",
+      elements: [
+        {
+          type: "mrkdwn",
+          text: `:date: *Date*: ${eventDate}\n:clock1: *Time*: ${startTime} - ${endTime}\n:round_pushpin: *Location*: ${event.location}`,
+        },
+      ],
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `${event.description}`,
+      }
+    },
+  ];
+  return eventBlock;
+};
+
+module.exports = {
+  createHeaderBlock,
+  createEventBlock,
+};
--- modules/calendar.js
+++ modules/calendar.js
@@ -1,9 +1,9 @@
 const { google } = require("googleapis");
+const { createHeaderBlock, createEventBlock } = require("../blocks/events");
 
 require("dotenv").config();
 
 const key = process.env.GCAL_API_KEY;
-
 const calendarId =
   "c_75cfdbedac5660fc90614ee4b59b7119142550b0bc91be5826ab40e7482d6b0c@group.calendar.google.com";
 
@@ -12,29 +12,9 @@ const calendar = google.calendar({
   auth: key,
 });
 
-// weekly event header
-headerBlock = [
-  {
-    type: "section",
-    text: {
-      type: "mrkdwn",
-      text: ":tada: *EVENTS THIS WEEK* :tada:",
-    },
-  },
-  {
-    type: "context",
-    elements: [
-      {
-        text: "*November 12, 2023* - *November 19, 2023*",
-        type: "mrkdwn",
-      },
-    ],
-  },
-];
+let section_blocks = [].concat(createHeaderBlock("Monday", "Friday"));
 
-let section_blocks = [].concat(headerBlock);
-
-// Function to retrieve upcoming events from the public Google Calendar
+// Retrieve upcoming events from the public Google Calendar
 const getUpcomingEvents = async () => {
   try {
     const response = await calendar.events.list({
@@ -52,73 +32,16 @@ const getUpcomingEvents = async () => {
   }
 };
 
+// Display messages into #events channel
 const printUpcomingEvents = async ({ ack, say }) => {
-  // acknowledge the action
   await ack();
 
-  // Get upcoming events from the Google Calendar
   const events = await getUpcomingEvents();
 
   if (events.length > 0) {
     events.forEach((event) => {
-      eventBlock = [
-        {
-          type: "divider",
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `*${event.summary}*`,
-          },
-          accessory: {
-            type: "button",
-            text: {
-              type: "plain_text",
-              emoji: true,
-              text: "RSVP",
-            },
-            value: "click_me_123",
-          },
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-          },
-        },
-        {
-          type: "context",
-          elements: [
-            {
-              type: "mrkdwn",
-              text: ":date: *Date*: " + `${event.start.date}`,
-            },
-          ],
-        },
-        {
-          type: "context",
-          elements: [
-            {
-              type: "mrkdwn",
-              text: ":clock1: *Time*: " + `${event.start.dateTime}`,
-            },
-          ],
-        },
-        {
-          type: "context",
-          elements: [
-            {
-              type: "mrkdwn",
-              text: ":round_pushpin: *Location*: " + `${event.location}`,
-            },
-          ],
-        },
-      ];
+      eventBlock = createEventBlock(event);
       section_blocks = section_blocks.concat(eventBlock);
-      //   const start = event.start.dateTime || event.start.date;
-      //   eventList += `${start} - ${event.summary}\n`;
     });
 
     await say({
